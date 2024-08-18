c392598efa11048d62fffbc1bc6cb144548b9609
karthik <karthiknadar1204@gmail.com>
2024-08-18 16:11:49 +0530

    initialise build with shadcn and clerk

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Page
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default page
